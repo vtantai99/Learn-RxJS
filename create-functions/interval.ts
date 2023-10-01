5a2fd7396e319c5b8b08a97cc2b376f6f65c46c1
Tai Vo <tantaivo74@gmail.com>
2023-10-01 15:07:56 +0700

    Create Function: interval

---
+++ create-functions/interval.ts
@@ -0,0 +1,7 @@
+// RxJS v6+
+import { interval } from 'rxjs';
+
+//emit value in sequence every 1 second
+const source = interval(1000);
+//output: 0,1,2,3,4,5....
+const subscribe = source.subscribe(val => console.log(val));
